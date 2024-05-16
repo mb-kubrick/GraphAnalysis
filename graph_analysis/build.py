f50abc4f443e97781e09a3ef0b79db38a3e5a9f0
Michael Berney <michaeljlb3@gmail.com>
2024-05-16 16:00:32 +0100

    basic functining graph

--- graph_analysis/build.py
+++ graph_analysis/build.py
@@ -1,10 +1,47 @@
+"""Functions for building the graph."""
+
+# IMPORTS --------------------------------------------------------------------------------------------------------------
+
 import os
 import json
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
 
-def build_graph(driver, raw_data, n_items = 100):
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------
 
+def set_driver():
+    """Defines the Neo4j Connection driver.
+
+    Returns:
+        driver: The driver used to connect to the database.
+    """
+    _ = load_dotenv()
+    url = os.getenv("URL")
+    username = os.getenv("NEO4J_USERNAME")
+    password = os.getenv("NEO4J_PASSWORD")
+    driver = GraphDatabase.driver(url, auth=(username, password))
+    return driver
+
+def connection_test(driver):
+    try:
+        return driver.verify_authentication()
+    except Exception as e:
+        print(f'Failed to connect to driver, reason: {e}')
+
+def close_driver(driver):
+    """Closes the supplied driver."""
+    driver.close()
+
+def read_data():
+    file_path = os.getcwd() + '/data/Summer22_FootballTransfers.json'
+
+    with open(file_path, "r", encoding='utf-8') as file:
+        data = json.load(file)
+
+    return data
+
+def build_graph(driver, raw_data, n_items = 100):
+    """Builds the graph from the supplied JSON data."""
     folder_path = os.getcwd() + '/graph_analysis/cypher/'
     file_names = ['clean_database.cypher', 'build.cypher']
 
@@ -27,34 +64,3 @@ def build_graph(driver, raw_data, n_items = 100):
 
         with driver.session() as session:
             result = session.run(query, **params)
-
-def set_driver():
-    """Defines the Neo4j Connection driver.
-
-    Returns:
-        driver: The driver used to connect to the database.
-    """
-    _ = load_dotenv()
-    url = os.getenv("URL")
-    username = os.getenv("NEO4J_USERNAME")
-    password = os.getenv("NEO4J_PASSWORD")
-    driver = GraphDatabase.driver(url, auth=(username, password))
-    return driver
-
-def close_driver(driver):
-    """Closes the supplied driver."""
-    driver.close()
-
-def read_data():
-    file_path = os.getcwd() + '/data/Summer22_FootballTransfers.json'
-
-    with open(file_path, "r", encoding='utf-8') as file:
-        data = json.load(file)
-
-    return data
-
-def connection_test(driver):
-    try:
-        return driver.verify_authentication()
-    except Exception as e:
-        print(f'Failed to connect to driver, reason: {e}')
