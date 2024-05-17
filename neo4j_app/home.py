19e5ddd4c287438f133cc22e8b7ed19fe6c333f6
fahima-a <fahima.ahm@outlook.com>
2024-05-17 16:59:08 +0100

    more changes

--- neo4j_app/home.py
+++ neo4j_app/home.py
@@ -9,10 +9,10 @@ from dijkstra import run_dijkstra
 from build1 import set_driver, close_driver, build_graph, read_data, connection_test
 import pandas as pd
 
+st.header("Summer 2022 Football Transfer ⚽")
+st.write("<span style='font-style: italic;'>Pod 1</span>", unsafe_allow_html=True)
 file_path = os.getcwd() + '/Summer22_FootballTransfers.csv'
 data = pd.read_csv(file_path)
-
-st.write(data['name'])
 
 player1 = st.selectbox('Select your first player', data['name'])
 player2 = st.selectbox('Select your second player', data['name'])
