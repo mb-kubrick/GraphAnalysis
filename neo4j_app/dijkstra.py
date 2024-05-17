18e58f7c4a5a76dca2d81872a899096f2fba25e4
Fahima Ahmed <fahimaahmed@kubrickgroup.com>
2024-05-17 14:44:34 +0100

    neo4j app

---
+++ neo4j_app/dijkstra.py
@@ -0,0 +1,39 @@
+"""Code to run the Dijkstra algorithm on the graph dataset."""
+
+# IMPORTS --------------------------------------------------------------------------------------------------------------
+
+import os
+import numpy as np
+
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------
+
+def run_dijkstra(players, driver) -> str:
+    """Determines the shortest path between nodes.
+
+    Args:
+        players (list): List of players that currently exist within the graph.
+        driver (driver): The driver instance which is connected to Neo4j.
+
+    Returns:
+        str: The shortest path between players, as a string.
+    """
+
+    player_1, player_2 = np.random.choice(players, replace=False, size=2)
+
+    folder_path = os.getcwd() + '/graph_analysis/cypher/'
+
+    file_names = ['dijkstra_clean.cypher', 'dijkstra_build.cypher', 'dijkstra_call.cypher']
+    all_params = [{}, {}, {'player_1_name': player_1, 'player_2_name': player_2}]
+
+    for file_name, params in zip(file_names, all_params):
+
+        with open(folder_path + file_name, "r", encoding='utf-8') as file:
+            query = file.read()
+
+        with driver.session() as session:
+            result = session.run(query, **params).data()
+
+    raw_path_data = result[0]['path']
+
+    return (f"\nThe shortest connection from '{player_1}' to '{player_2}' is:"
+            + f"\n{' -> '.join([i['name'] for i in raw_path_data])}\n")
---
+++ neo4j_app/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from streamlit_extras.chart_container import chart_container
+
+st.balloons()
+st.write('hi')
